05e77204b715b069a567be77acbd57b48d8f9dff
Andrew <pensive.pink@gmail.com>
2022-12-11 11:48:48 -0500

    add zsh, vimrc, omz dotfiles

---
+++ oh-my-zsh/my_aliases.zsh
@@ -0,0 +1,9 @@
+alias gitlog='git log --graph --oneline'
+
+export DEVKITPRO=/opt/devkitpro
+export DEVKITARM=${DEVKITPRO}/devkitARM
+export DEVKITPPC=${DEVKITPRO}/devkitPPC
+export PATH=${DEVKITPRO}/tools/bin:$PATH
+
+alias up='cd ..'
+alias home='cd ~'
