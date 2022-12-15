aa611cc66f3e59af7c84c21077bd92198a19aa51
Parakeet <pensive0parakeet@gmail.com>
2022-12-14 22:44:55 -0500

    finish script, fix aliases

--- stow_script.sh
+++ stow_script.sh
@@ -1,5 +1,10 @@
-# Incomplete
+if [ -d "/usr/bin/zsh" ]
+then
+    #!/usr/bin/zsh"
+else
+    #!/bin/zsh
+fi
 
-stow --adopt -t ~/.oh-my-zsh/custom ~/.dotfiles/oh-my-zsh
+stow --adopt -t ~/.oh-my-zsh/custom oh-my-zsh
 stow --adopt vim
 stow --adopt zsh
